8277e28327d00cdb520aabf4ff7d69ad8bec84a0
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-08-23 17:41:36 +0300

    tabs commited

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
-}
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h2>Loading..</h2>
+      </section>
+    )
+  }
+
+  const { id, title, dates, duties, company } = jobs[value]
+
+
+  return (
+  <section className="section">
+    <div className="title">
+      <h2>Expierence</h2>
+      <div className="underlinde"></div>
+    </div>
+    <div className="jobs-center">
+    <article className="job-info">
+      <h3>{title}</h3>
+      <h4>{company}</h4>
+      <p className="job-date">{dates}</p>
+      {duties.map((duty, index) => {
+        return (
+          <div key={index} className="job-desc">
+            <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+            {duty}
+          </div>
+        )
+      })
+      }
+    </article>
+    </div>
+  </section>
+  )}
 
 export default App
