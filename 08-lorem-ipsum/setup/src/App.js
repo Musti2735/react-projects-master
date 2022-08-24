93ad9d00d84070628a8ab2d85d9060d123351386
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-08-24 17:56:06 +0300

    lorem ipsum commited

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>generate</label>
+        <input
+          type="number"
+          name='amount'
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button className='btn'>generate</button>
+      </form>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+
+    </section>
+  )
 }
 
 export default App;
