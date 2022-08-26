83fd1f557e2b6457e01ace0335b29000d8319d43
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-08-26 12:22:16 +0300

    commited

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -34,6 +34,10 @@ export default SingleColor
 
 /*
 
-props olarak, app.js'den gelen color objesi içinde rgb weight ve index propslarını ayrı ayrı aldık. hexColor ise app.jsde tanımladığmız başka bir değişken 
+props olarak, app.js'den gelen color objesi içinde rgb weight ve index propslarını ayrı ayrı aldık. hexColor ise app.jsde tanımladığmız başka bir değişken. Bu değişken ile hex kodunu sayfada kullanıcıya sunacağız.
+
+propsları article içinde kullanarak ekrana yazdırdık. rgb propsunu string haline getirerek bcg değişkenine atadık. her bir componentin bacground değerini template litarels içinde tanımlayarak bcg değerini buruda kullandık. Böylelikle her bir rgbye göre farklı renkleri ekrana getirmiş olduk. index değerini if koşuluna sokarak eğer index 10dan büyükse yani arkaplan rengi koyu ise üzerindeki yazıların renginin beyaza olmasını sağladık.
+
+article öğesinin üzerine tıklandığında hex kodunun kopyalanabilmesi için alert değişkeni tanımladık. alertin varsayılan değeri false. eğer alert true ise kopyalandı yazısını kutucuğa getirdik. Ve ardından timeOut ile 3 sn sonra kopyalandı yazısının kaldırılmasını sağladık. 
 
 */
