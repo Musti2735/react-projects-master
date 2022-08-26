d18bd26d74eb3ec16cf61e545246c4073840ff8d
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-08-26 17:48:14 +0300

    grocery bud commited

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,63 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { Button } from 'bootstrap';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [list, setList] = useState([]);
+  const [name, setName] = useState("");
+  const [isEditing, setIsEditing] = useState(false)
+  const [editId, setEditId] = useState(null)
+  const [alert, setAlert] = useState({msg:"", show:false, type:''});
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!name){ //eğer isim yoksa alert
+      // display alert
+    }
+    else if(name && isEditing){ // eğer isim var ve editing ture ise edit
+  
+    }
+    else{
+      // show alert
+      // create new item
+      const newItem={id: new Date().getTime().toString(), title: name} // kullanıcının girdiği text'i alıp eşsiz id oluşturarak bir obj içine koyduk.
+      setList([...list, newItem]) //list öğesine aldık ve newItemı ekledik.
+      setName("") //subnitten sonra input değerini sıfırladık
+   
+
+      
+    }
+
+  }
+
+  return (
+    <section className="section-center">
+      <form className="gorcery-form" onSubmit={handleSubmit} >
+      {alert.show && <Alert/>}
+      <h3>Grocery Bud</h3>
+      <div className="form-control">
+      <input 
+      type="text" 
+      className="grocery"
+      value={name}
+      onChange={(e)=>setName(e.target.value)} //hedefteki değeri name değişkenine atadık.
+      placeholder = 'Eggs..'
+      />
+      <button type="submit" className="submit-btn">
+        {isEditing ? "edit" : "submit"}
+      </button>
+      </div>
+
+      </form>
+      {list.length>0 &&( //liste boş değilse clear items butonunu göster
+      <div className="grocery-container">
+      <List items={list}/>
+      <button className="clear-btn">Clear Items</button>
+    </div>
+      )}
+    </section>
+  )
 }
 
 export default App
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+import { CardTitle } from 'reactstrap'
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { title, id } = item;
+        return (
+          <article key={id} className='grocery-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <FaEdit className='edit-btn' />
+              <FaTrash className='delete-btn' />
+            </div>
+          </article>
+        )
+
+      })}
+    </div>
+  )
 }
 
 export default List
